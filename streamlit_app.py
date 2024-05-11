faf6d922145ce609a7268b1662a5899949196062
ajs177 <168984931+ajs177@users.noreply.github.com>
2024-05-11 16:01:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import os
 import streamlit as st
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.docstore.document import Document
-from langchain_core import PromptTemplate
 from langchain.chains.summarize import load_summarize_chain
 from langchain_community.llms import CTransformers
 from langchain.callbacks.manager import CallbackManager
